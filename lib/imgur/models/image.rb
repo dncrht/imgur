87b7bfa9e82e98353b2261c538c2dca2ee43adbc
Dani <daniel.cr.ht@gmail.com>
2015-07-04 07:34:58 +0100

    Generate link from id for new images

--- lib/imgur/models/image.rb
+++ lib/imgur/models/image.rb
@@ -4,6 +4,10 @@ module Imgur
     IMAGE_URL = 'http://i.imgur.com/'
     IMAGE_EXTENSION = 'jpg' # jpg is the default extension for every Imgur image
 
+    def link
+      @link ||= "#{IMAGE_URL}#{id}.#{IMAGE_EXTENSION}"
+    end
+
     # Provides the download URL in case you know a valid imgur hash and don't want to make a network trip with .find
     # Just in case you don't need the full Imgur::Image object
     def url(size)
